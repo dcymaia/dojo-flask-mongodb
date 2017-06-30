3c96cd40d757281e06bb2aae4cc9b8abf1110af4
Danilo Cyrino Maia <dcymaia@gmail.com>
2017-06-30 18:14:10 -0300

    wip

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
-
 my_app_web = Flask('my_app_web')
 my_mongo = PyMongo(my_app_web)
+
+
+@my_app_web.route('/')
+def home_page():
+    online_users = my_mongo.db.users.find({'online': True})
+    return render_template('index.html',
+                           online_users=online_users)
 
 
 @my_app_web.route('/list')
--- tests.py
+++ tests.py
@@ -3,6 +3,11 @@ from app import my_app_web
 
 
 class TestApp(unittest.TestCase):
+    def test_app_home_url(self):
+        app = my_app_web.test_client()
+        self.response = app.get('/')
+        self.assertEqual(200, self.response.status_code)
+
     def test_app_list_url(self):
         app = my_app_web.test_client()
         self.response = app.get('/list')
